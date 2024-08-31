f3df32751f8e969bea0f0072fcbadefdb14061a9
Asawin <asawin.krongprasert@outlook.com>
2024-08-31 20:06:25 +0700

    Add project to repo.

---
+++ PleaseDoTutorial/PleaseDoTutorial/PleaseDoTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  PleaseDoTutorialApp.swift
+//  PleaseDoTutorial
+//
+//  Created by Asawin Krongprasert on 31/8/24.
+//
+
+import SwiftUI
+
+@main
+struct PleaseDoTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
