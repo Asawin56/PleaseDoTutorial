23360d4f2ddb5e9c32b146901601af09ace55e21
Asawin Krongprasert <asawin.krongprasert@outlook.com>
2025-05-04 20:02:10 +0700

    Do more

--- PleaseDoTutorial/Models/Item.swift
+++ PleaseDoTutorial/Models/Item.swift
@@ -13,7 +13,8 @@ struct Item: Identifiable, Equatable, Hashable {
     var title: String
     var description: String
     var startDate: Date = .now
-    
+    var status: Status
+    var priority: Priority
 }
 
 enum Status: String, CaseIterable {
