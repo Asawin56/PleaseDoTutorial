a655052e9c8681ee810ae9a60793d2a9a2b2a09c
Asawin Krongprasert <asawin.krongprasert@outlook.com>
2025-05-04 13:46:23 +0700

    Add ListView to project.

---
+++ PleaseDoTutorial/Views/ListView.swift
@@ -0,0 +1,18 @@
+//
+//  ListView.swift
+//  PleaseDoTutorial
+//
+//  Created by Asawin Krongprasert on 4/5/2568 BE.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListView()
+}
