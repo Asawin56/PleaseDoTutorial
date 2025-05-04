0aed21cce30ddc3e82c1e2294f4b81ddd0268982
Asawin Krongprasert <asawin.krongprasert@outlook.com>
2025-05-05 00:35:01 +0700

    Add LoginView

---
+++ PleaseDoTutorial/Views/Login/LoginTitleView.swift
@@ -0,0 +1,38 @@
+//
+//  LoginTitleView.swift
+//  PleaseDoTutorial
+//
+//  Created by Asawin Krongprasert on 5/5/2568 BE.
+//
+
+import SwiftUI
+
+struct LoginTitleView: View {
+    var body: some View {
+        HStack {
+            Image("PleaseDoLogo")
+                .resizable()
+                .scaledToFit()
+            
+            VStack(spacing: 3) {
+                Text("Please Do")
+                    .font(.title)
+                    .fontWeight(.semibold)
+                
+                RoundedRectangle(cornerRadius: 5)
+                    .frame(maxWidth: 175, maxHeight: 0.7)
+                
+                Text("A better todo list")
+                    .font(.subheadline)
+                    .fontWeight(.medium)
+                    .foregroundStyle(.gray)
+            }
+        }
+        .frame(maxHeight: 110)
+        .padding(.horizontal)
+    }
+}
+
+#Preview {
+    LoginTitleView()
+}
---
+++ PleaseDoTutorial/Views/Login/LoginView.swift
@@ -0,0 +1,25 @@
+//
+//  LoginView.swift
+//  PleaseDoTutorial
+//
+//  Created by Asawin Krongprasert on 5/5/2568 BE.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 15) {
+            Spacer()
+            
+            LoginTitleView()
+            
+            Spacer()
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    LoginView()
+}
