840015a7d16a8f815b70a18309514bb176957d23
Asawin Krongprasert <asawin.krongprasert@outlook.com>
2025-05-05 14:07:47 +0700

    Firebase

---
+++ PleaseDoTutorial/App/LoginManager.swift
@@ -0,0 +1,14 @@
+//
+//  LoginManager.swift
+//  PleaseDoTutorial
+//
+//  Created by Asawin Krongprasert on 5/5/2568 BE.
+//
+
+import Foundation
+import FirebaseAuth
+import FirebaseFirestore
+
+final class LoginManager {
+    
+}
